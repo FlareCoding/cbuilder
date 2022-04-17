c9ed9e6b63a49410c9f82cb126bd84cba3c46dba
FlareCoding <albert.slepak@gmail.com>
2022-04-17 13:49:01 -0400

    fixed component card buttons

--- cbuilder/src/project_builder.js
+++ cbuilder/src/project_builder.js
@@ -2,13 +2,39 @@
 var components = []
 
 function addApplicationComponent() {
-    var newComponentName = 'component_' + String(components.length);
-    components.push(newComponentName);
+    var newComponentName = 'component_' + String(components.length + 1);
+    components.push({
+        componentName: newComponentName,
+        componentClasses: []
+    });
     
+    var componentCardNameTextbox = document.createElement('input');
+    componentCardNameTextbox.classList.add('card-name-textbox');
+    componentCardNameTextbox.value = newComponentName;
+    componentCardNameTextbox.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+    componentCardNameTextbox.addEventListener('keyup', function(e) {
+        components[components.length - 1].componentName = e.target.value;
+    });
+
     var componentCardButton = document.createElement('button');
     componentCardButton.classList.add('card-button');
     componentCardButton.role = 'button';
-    componentCardButton.innerHTML = newComponentName;
+    componentCardButton.appendChild(componentCardNameTextbox);
+    componentCardButton.addEventListener('click', function(e) {
+        console.log('clicked: ' + e.target.childNodes[0].value);
+    });
+    componentCardNameTextbox.addEventListener('keypress', function (e) {
+        if (e.key === ' ') {
+            e.preventDefault();
+        }
+    });
+    componentCardNameTextbox.addEventListener('keyup', function (e) {
+        if (e.key === ' ') {
+            e.preventDefault();
+        }
+    });
 
     var componentCard = document.createElement('div');
     componentCard.classList.add('item-card');
